63436fccfebcea2213f3ea8bda382ddd380599e4
faoue300-oss <faoue300@gmail.com>
2026-09-20 11:53:28 +0000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,35 @@
+/* Noritgen PWA Service Worker */
+const CACHE = 'noritgen-v2';
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) =>
+      cache.addAll(['./', './manifest.json', './icons/icon-192.png', './icons/icon-512.png']).catch(() => {})
+    ).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  const req = event.request;
+  if (req.method !== 'GET') return;
+  event.respondWith(
+    caches.match(req).then((cached) => {
+      const fetched = fetch(req).then((res) => {
+        if (res && res.ok && req.url.startsWith(self.location.origin)) {
+          const clone = res.clone();
+          caches.open(CACHE).then((cache) => cache.put(req, clone));
+        }
+        return res;
+      }).catch(() => cached);
+      return cached || fetched;
+    })
+  );
+});
